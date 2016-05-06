c44cfe822f9d7a622a370e7b79a2be8fbeae8710
Yortw <troywillmot@gmail.com>
2016-05-06 21:44:37 +1200

    Create recycling text writer and tests.

---
+++ ScribeSharp.Shared.Contracts/RecyclableStringWriter.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScribeSharp
+{
+	/// <summary>
+	/// A <see cref="System.IO.StringWriter"/> implementation that can be reused, reducing the number of allocations for both the writers and underlying string builders.
+	/// </summary>
+	/// <remarks>
+	/// <para>When the <see cref="Close()"/> or <see cref="Dispose(bool)"/> methods are called the underlying string builder is reset to a length of zero so it is ready to be used for a new string.</para>
+	/// <para>If, when reset, the string builder capacity is over a specified maximum, it will be reset to a specified default size. This can help avoid keeping huge string builder buffers in memory.</para>
+	/// </remarks>
+	public class RecyclableStringWriter : System.IO.StringWriter
+	{
+
+		#region Fields
+
+		private StringBuilder _StringBuilder;
+		private int _MaxCapacity;
+		private int _DefaultCapacity;
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RecyclableStringWriter"/> class, with no maximum or default capacity set.
+		/// </summary>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1305:SpecifyIFormatProvider", MessageId = "System.IO.StringWriter.#ctor")]
+		public RecyclableStringWriter() : base()
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RecyclableStringWriter"/> class, with the specified format control and no maximum or default capacity set.
+		/// </summary>
+		/// <param name="formatProvider">An <see cref="IFormatProvider"/> object that controls formatting.</param>
+		public RecyclableStringWriter(IFormatProvider formatProvider) : base(formatProvider)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RecyclableStringWriter"/> class that will write to the specified <see cref="StringBuilder"/>. The <see cref="StringBuilder.MaxCapacity"/> is used for he maximum capacity and the <see cref="StringBuilder.Capacity"/> values from the provided string builder are used when this instance is recycled.
+		/// </summary>
+		/// <param name="stringBuilder">The <see cref="StringBuilder"/> to write to.</param>
+		/// <exception cref="ArgumentNullException">Thrown if the <paramref name="stringBuilder"/> is null.</exception>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1305:SpecifyIFormatProvider", MessageId = "System.IO.StringWriter.#ctor(System.Text.StringBuilder)")]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1720:IdentifiersShouldNotContainTypeNames", MessageId = "string")]
+		public RecyclableStringWriter(StringBuilder stringBuilder) : base(stringBuilder)
+		{
+			if (stringBuilder == null) throw new ArgumentNullException(nameof(stringBuilder));
+
+			_MaxCapacity = stringBuilder.MaxCapacity;
+			_DefaultCapacity = stringBuilder.Capacity;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RecyclableStringWriter"/> class that will write to the specified <see cref="StringBuilder"/>. The <see cref="StringBuilder.MaxCapacity"/> is used for he maximum capacity and the <see cref="StringBuilder.Capacity"/> values from the provided string builder are used when this instance is recycled.
+		/// </summary>
+		/// <param name="stringBuilder">The <see cref="StringBuilder"/> to write to.</param>
+		/// <param name="formatProvider">An <see cref="IFormatProvider"/> object that controls formatting.</param>
+		/// <exception cref="ArgumentNullException">Thrown if the <paramref name="stringBuilder"/> is null.</exception>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1720:IdentifiersShouldNotContainTypeNames", MessageId = "string")]
+		public RecyclableStringWriter(StringBuilder stringBuilder, IFormatProvider formatProvider) : base(stringBuilder, formatProvider)
+		{
+			if (stringBuilder == null) throw new ArgumentNullException(nameof(stringBuilder));
+
+			_MaxCapacity = stringBuilder.MaxCapacity;
+			_DefaultCapacity = stringBuilder.Capacity;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RecyclableStringWriter"/> class that will write to the specified <see cref="StringBuilder"/>. The <see cref="StringBuilder.MaxCapacity"/> is used for he maximum capacity and the <see cref="StringBuilder.Capacity"/> values from the provided string builder are used when this instance is recycled.
+		/// </summary>
+		/// <param name="stringBuilder">The <see cref="StringBuilder"/> to write to.</param>
+		/// <param name="formatProvider">An <see cref="IFormatProvider"/> object that controls formatting.</param>
+		/// <param name="maxCapacity">The maximum number of characters allows in the <see cref="StringBuilder"/> before it's capacity is reset to <paramref name="defaultCapacity"/> when recycled.</param>
+		/// <param name="defaultCapacity">The number of characters to reset the <see cref="StringBuilder"/> capacity to when recycled (if it is larger than the <paramref name="maxCapacity" /> argument).</param>
+		/// <exception cref="ArgumentNullException">Thrown if the <paramref name="stringBuilder"/> is null.</exception>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1720:IdentifiersShouldNotContainTypeNames", MessageId = "string")]
+		public RecyclableStringWriter(StringBuilder stringBuilder, IFormatProvider formatProvider, int maxCapacity, int defaultCapacity) : base(stringBuilder, formatProvider)
+		{
+			_MaxCapacity = maxCapacity;
+			_DefaultCapacity = defaultCapacity;
+		}
+
+		#endregion
+
+		#region Overrides
+
+		/// <summary>
+		/// Does NOT close the text writer or underlying <see cref="System.Text.StringBuilder"/>, but instead resets it so it can be reused.
+		/// </summary>
+		public override void Close()
+		{
+			ResetStringBuilder();
+		}
+
+		/// <summary>
+		/// Really disposes this instance instead of just resetting it. Should only be called if you do not intend to use this instance again.
+		/// </summary>
+		public void ForceDispose()
+		{
+			base.Dispose(true);
+		}
+
+		/// <summary>
+		/// If <paramref name="disposing"/> is true, resets the underlying string builder for use. If false, calls the base dispose method which will perform a full clean up.
+		/// </summary>
+		/// <param name="disposing">A boolean indicating whether this instance is being disposed explicitly (true), or from the finalizer (false).</param>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2215:Dispose methods should call base class dispose")]
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+				ResetStringBuilder();
+			else
+				base.Dispose();
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private void ResetStringBuilder()
+		{
+			if (_StringBuilder == null)
+				_StringBuilder = this.GetStringBuilder();
+
+			_StringBuilder.Length = 0;
+			if (_StringBuilder.Capacity > _MaxCapacity && _MaxCapacity > 0 && _DefaultCapacity > 0)
+				_StringBuilder.Capacity = _DefaultCapacity;
+		}
+
+		#endregion
+
+	}
+}
---
+++ ScribeSharp.Tests/RecyclingTextWriterTests.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScribeSharp.Tests
+{
+	[TestClass]
+	public class RecyclingTextWriterTests
+	{
+
+		#region Constructors
+
+		[TestMethod]
+		public void RecyclingTextWriter_Constructor_ConstructsOk()
+		{
+			var writer = new RecyclableStringWriter();
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void RecyclingTextWriter_Constructor_ThrowsOnNullStringBuilder()
+		{
+			var writer = new RecyclableStringWriter((StringBuilder)null);
+		}
+
+		[TestMethod]
+		public void RecyclingTextWriter_Constructor_AllowsNullFormatProvider()
+		{
+			var writer = new RecyclableStringWriter((IFormatProvider)null);
+		}
+
+		[TestMethod]
+		public void RecyclingTextWriter_Constructor_ConstructsOkWithStringBuilderAndFormatProvider()
+		{
+			var sb = new StringBuilder();
+			var writer = new RecyclableStringWriter(sb, System.Globalization.CultureInfo.CurrentCulture);
+		}
+
+		[TestMethod]
+		public void RecyclingTextWriter_Constructor_FullConstructorConstructsOk()
+		{
+			var sb = new StringBuilder();
+			var writer = new RecyclableStringWriter(sb, System.Globalization.CultureInfo.CurrentCulture, 1024, 512);
+		}
+
+		#endregion
+
+		#region Dispose Tests
+
+		[TestMethod]
+		public void RecyclingTextWriter_Dispose_DoesNotDisposeInstance()
+		{
+			var sb = new System.Text.StringBuilder();
+			var writer = new RecyclableStringWriter(sb);
+
+			writer.Dispose();
+
+			writer.Write("Test");
+		}
+
+		[TestMethod]
+		public void RecyclingTextWriter_Dispose_ResetsStringBuilder()
+		{
+			var sb = new System.Text.StringBuilder();
+			var writer = new RecyclableStringWriter(sb);
+
+			writer.Write("Test");
+			Assert.AreEqual("Test", sb.ToString());
+
+			writer.Dispose();
+
+			Assert.AreEqual(0, sb.Length);
+			Assert.AreEqual(String.Empty, sb.ToString());
+		}
+
+		[TestMethod]
+		public void RecyclingTextWriter_Dispose_SetsCapacityWhenOverSize()
+		{
+			var sb = new System.Text.StringBuilder();
+			var writer = new RecyclableStringWriter(sb, System.Globalization.CultureInfo.CurrentCulture, 1024, 512);
+
+			writer.Write(new string('A', 2048));
+			writer.Dispose();
+
+			Assert.AreEqual(512, sb.Capacity);
+			Assert.AreEqual(0, sb.Length);
+			Assert.AreEqual(String.Empty, sb.ToString());
+		}
+
+		[TestMethod]
+		public void RecyclingTextWriter_Dispose_DoesNotSetCapacityWhenUnderMaxSize()
+		{
+			var sb = new System.Text.StringBuilder();
+			var writer = new RecyclableStringWriter(sb);
+
+			writer.Write(new string('A', 600));
+			writer.Dispose();
+
+			Assert.AreEqual(600, sb.Capacity);
+			Assert.AreEqual(0, sb.Length);
+			Assert.AreEqual(String.Empty, sb.ToString());
+		}
+
+		#endregion
+
+		#region Close Tests
+
+		[TestMethod]
+		public void RecyclingTextWriter_Close_DoesNotCloseInstance()
+		{
+			var sb = new System.Text.StringBuilder();
+			var writer = new RecyclableStringWriter(sb);
+
+			writer.Close();
+
+			writer.Write("Test");
+		}
+
+		[TestMethod]
+		public void RecyclingTextWriter_Close_ResetsStringBuilder()
+		{
+			var sb = new System.Text.StringBuilder();
+			var writer = new RecyclableStringWriter(sb);
+
+			writer.Write("Test");
+			Assert.AreEqual("Test", sb.ToString());
+
+			writer.Close();
+
+			Assert.AreEqual(0, sb.Length);
+			Assert.AreEqual(String.Empty, sb.ToString());
+		}
+
+		[TestMethod]
+		public void RecyclingTextWriter_Close_SetsCapacityWhenOverSize()
+		{
+			var sb = new System.Text.StringBuilder();
+			var writer = new RecyclableStringWriter(sb, System.Globalization.CultureInfo.CurrentCulture, 1024, 512);
+
+			writer.Write(new string('A', 2048));
+			writer.Close();
+
+			Assert.AreEqual(512, sb.Capacity);
+			Assert.AreEqual(0, sb.Length);
+			Assert.AreEqual(String.Empty, sb.ToString());
+		}
+
+		[TestMethod]
+		public void RecyclingTextWriter_Close_DoesNotSetCapacityWhenUnderMaxSize()
+		{
+			var sb = new System.Text.StringBuilder();
+			var writer = new RecyclableStringWriter(sb);
+
+			writer.Write(new string('A', 600));
+			writer.Close();
+
+			Assert.AreEqual(600, sb.Capacity);
+			Assert.AreEqual(0, sb.Length);
+			Assert.AreEqual(String.Empty, sb.ToString());
+		}
+
+		#endregion
+
+		#region Force Dispose Tests
+
+		[TestMethod]
+		[ExpectedException(typeof(ObjectDisposedException))]
+		public void RecyclingTextWriter_ForceDispose_ReallyDisposes()
+		{
+			var writer = new RecyclableStringWriter();
+			writer.ForceDispose();
+			writer.Write("Test");
+		}
+
+		#endregion
+
+	}
+}
